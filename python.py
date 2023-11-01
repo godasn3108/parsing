7ed0e70088a93193557b9e6c192d0dcf2a403cb6
godasn3108 <112085822+godasn3108@users.noreply.github.com>
2023-11-02 01:24:42 +0530

    Update python.py

--- python.py
+++ python.py
@@ -4,8 +4,11 @@ import json
 with open('input.yaml', 'r')as file:
 	data = yaml.safe_load(file)
 
-jsonStrData = json.dumps(data) 
-print(jsonStrData)
+#jsonStrData = json.dumps(data) 
+#print(jsonStrData)
+
+for key, value in data.items();
+	os.environ[key] = str(value)'
 
 #Set Output Variable
 #output_value = data.get('R_VERSION')
