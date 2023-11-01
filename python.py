58e2d8488bf01e8d3afcd2db8cefd691e20741d6
godasn3108 <112085822+godasn3108@users.noreply.github.com>
2023-11-02 01:12:29 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,8 +1,12 @@
 import yaml
+import json
 
 with open('input.yaml', 'r')as file:
 	data = yaml.safe_load(file)
-print(data)
+
+jsonStrData = json.dumps(data) 
+print(jsonStrData)
+
 #Set Output Variable
 #output_value = data.get('R_VERSION')
-print(f"::set-output name=data::{data}")
+print(f"::set-output name=data::{jsonStrData}")
